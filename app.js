d87609eb87178b73196b9350410d522abee9f92c
Naib Rahman <naib.rahman@gmail.com>
2022-01-25 16:26:17 +0000

    foreach for numbers & operators

--- app.js
+++ app.js
@@ -1,53 +1,84 @@
-const display = document.getElementById("console__display")
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
-const zero = document.getElementById("0");
+let display = document.getElementById("console__display");
+const numbers = document.querySelectorAll(".btn__number");
+const operators = document.querySelectorAll(".btn__operator")
+// const one = document.getElementById("1");
+// const two = document.getElementById("2");
+// const three = document.getElementById("3");
+// const four = document.getElementById("4");
+// const five = document.getElementById("5");
+// const six = document.getElementById("6");
+// const seven = document.getElementById("7");
+// const eight = document.getElementById("8");
+// const nine = document.getElementById("9");
+// const zero = document.getElementById("0");
+const clear = document.getElementById("clear");
+let firstNumber = (0);
 
+// for (let i=0; i<numbers.length; i++) {
+//     numbers[i].addEventListener("click", (event) => {
+//         display.innerHTML += numbers[i].innerHTML;
+//     })
+// }
 
-one.addEventListener("click", (event) => {
-    display.innerHTML += 1;
+numbers.forEach(number => {
+    number.addEventListener("click", (event) => {
+        display.innerHTML += number.innerHTML;
+    })
 })
 
-two.addEventListener("click", (event) => {
-    display.innerHTML += 2;
+operators.forEach(operator => {
+    operator.addEventListener("click", (event) => {
+        firstNumber = display
+        display.innerHTML += operator.innerHTML;
+    })
 })
 
-three.addEventListener("click", (event) => {
-    display.innerHTML += 3;
-})
 
-four.addEventListener("click", (event) => {
-    display.innerHTML += 4;
-})
 
-five.addEventListener("click", (event) => {
-    display.innerHTML += 5;
-})
+// one.addEventListener("click", (event) => {
+//     display.innerHTML += 1;
+// })
 
-six.addEventListener("click", (event) => {
-    display.innerHTML += 6;
-})
+// two.addEventListener("click", (event) => {
+//     display.innerHTML += 2;
+// })
 
-seven.addEventListener("click", (event) => {
-    display.innerHTML += 7;
-})
+// three.addEventListener("click", (event) => {
+//     display.innerHTML += 3;
+// })
 
-eight.addEventListener("click", (event) => {
-    display.innerHTML += 8;
-})
+// four.addEventListener("click", (event) => {
+//     display.innerHTML += 4;
+// })
 
-nine.addEventListener("click", (event) => {
-    display.innerHTML += 9;
-})
+// five.addEventListener("click", (event) => {
+//     display.innerHTML += 5;
+// })
 
-zero.addEventListener("click", (event) => {
-    display.innerHTML += 0;
-})
+// six.addEventListener("click", (event) => {
+//     display.innerHTML += 6;
+// })
 
+// seven.addEventListener("click", (event) => {
+//     display.innerHTML += 7;
+// })
+
+// eight.addEventListener("click", (event) => {
+//     display.innerHTML += 8;
+// })
+
+// nine.addEventListener("click", (event) => {
+//     display.innerHTML += 9;
+// })
+
+// zero.addEventListener("click", (event) => {
+//     display.innerHTML += 0;
+// })
+
+// clear.addEventListener("click", (event) => {
+//     display.innerHTML = " ";
+// })
+
+clear.addEventListener("click", (event) => {
+    display.innerHTML = " ";
+})
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,43 +1,69 @@
 "use strict";
 
 var display = document.getElementById("console__display");
-var one = document.getElementById("1");
-var two = document.getElementById("2");
-var three = document.getElementById("3");
-var four = document.getElementById("4");
-var five = document.getElementById("5");
-var six = document.getElementById("6");
-var seven = document.getElementById("7");
-var eight = document.getElementById("8");
-var nine = document.getElementById("9");
-var zero = document.getElementById("0");
-one.addEventListener("click", function (event) {
-  display.innerHTML += 1;
+var numbers = document.querySelectorAll(".btn__number");
+var operators = document.querySelectorAll(".btn__operator"); // const one = document.getElementById("1");
+// const two = document.getElementById("2");
+// const three = document.getElementById("3");
+// const four = document.getElementById("4");
+// const five = document.getElementById("5");
+// const six = document.getElementById("6");
+// const seven = document.getElementById("7");
+// const eight = document.getElementById("8");
+// const nine = document.getElementById("9");
+// const zero = document.getElementById("0");
+
+var clear = document.getElementById("clear");
+var firstNumber = 0; // for (let i=0; i<numbers.length; i++) {
+//     numbers[i].addEventListener("click", (event) => {
+//         display.innerHTML += numbers[i].innerHTML;
+//     })
+// }
+
+numbers.forEach(function (number) {
+  number.addEventListener("click", function (event) {
+    display.innerHTML += number.innerHTML;
+  });
 });
-two.addEventListener("click", function (event) {
-  display.innerHTML += 2;
-});
-three.addEventListener("click", function (event) {
-  display.innerHTML += 3;
-});
-four.addEventListener("click", function (event) {
-  display.innerHTML += 4;
-});
-five.addEventListener("click", function (event) {
-  display.innerHTML += 5;
-});
-six.addEventListener("click", function (event) {
-  display.innerHTML += 6;
-});
-seven.addEventListener("click", function (event) {
-  display.innerHTML += 7;
-});
-eight.addEventListener("click", function (event) {
-  display.innerHTML += 8;
-});
-nine.addEventListener("click", function (event) {
-  display.innerHTML += 9;
-});
-zero.addEventListener("click", function (event) {
-  display.innerHTML += 0;
+operators.forEach(function (operator) {
+  operator.addEventListener("click", function (event) {
+    firstNumber = display;
+    display.innerHTML += operator.innerHTML;
+  });
+}); // one.addEventListener("click", (event) => {
+//     display.innerHTML += 1;
+// })
+// two.addEventListener("click", (event) => {
+//     display.innerHTML += 2;
+// })
+// three.addEventListener("click", (event) => {
+//     display.innerHTML += 3;
+// })
+// four.addEventListener("click", (event) => {
+//     display.innerHTML += 4;
+// })
+// five.addEventListener("click", (event) => {
+//     display.innerHTML += 5;
+// })
+// six.addEventListener("click", (event) => {
+//     display.innerHTML += 6;
+// })
+// seven.addEventListener("click", (event) => {
+//     display.innerHTML += 7;
+// })
+// eight.addEventListener("click", (event) => {
+//     display.innerHTML += 8;
+// })
+// nine.addEventListener("click", (event) => {
+//     display.innerHTML += 9;
+// })
+// zero.addEventListener("click", (event) => {
+//     display.innerHTML += 0;
+// })
+// clear.addEventListener("click", (event) => {
+//     display.innerHTML = " ";
+// })
+
+clear.addEventListener("click", function (event) {
+  display.innerHTML = " ";
 });
