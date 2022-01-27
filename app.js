79515aa6fd8701a72691fd4bfd0a9a2156336340
Naib Rahman <naib.rahman@gmail.com>
2022-01-27 15:24:37 +0000

    if statements

--- app.js
+++ app.js
@@ -1,84 +1,74 @@
-let display = document.getElementById("console__display");
+const display = document.getElementById("console__display");
+const totalDisplay = document.getElementById("console__total");
+
+
 const numbers = document.querySelectorAll(".btn__number");
 const operators = document.querySelectorAll(".btn__operator")
-// const one = document.getElementById("1");
-// const two = document.getElementById("2");
-// const three = document.getElementById("3");
-// const four = document.getElementById("4");
-// const five = document.getElementById("5");
-// const six = document.getElementById("6");
-// const seven = document.getElementById("7");
-// const eight = document.getElementById("8");
-// const nine = document.getElementById("9");
-// const zero = document.getElementById("0");
 const clear = document.getElementById("clear");
-let firstNumber = (0);
+const allClear = document.getElementById("allClear");
 
-// for (let i=0; i<numbers.length; i++) {
-//     numbers[i].addEventListener("click", (event) => {
-//         display.innerHTML += numbers[i].innerHTML;
-//     })
-// }
+const total = document.getElementById("total");
+
+let firstNumber;
+let secondNumber;
+let equals;
+let symbol;
+
 
 numbers.forEach(number => {
     number.addEventListener("click", (event) => {
         display.innerHTML += number.innerHTML;
+        if (firstNumber) {
+            secondNumber = display.innerHTML
+        } else {
+            firstNumber = display.innerHTML
+        }
     })
 })
 
 operators.forEach(operator => {
     operator.addEventListener("click", (event) => {
-        firstNumber = display
-        display.innerHTML += operator.innerHTML;
+        totalDisplay.innerHTML = firstNumber;
+        totalDisplay.innerHTML += operator.innerHTML;
+        console.log(operator.innerHTML);
+        symbol = operator.innerHTML;
+        display.innerHTML = " ";
     })
 })
 
 
 
-// one.addEventListener("click", (event) => {
-//     display.innerHTML += 1;
-// })
-
-// two.addEventListener("click", (event) => {
-//     display.innerHTML += 2;
-// })
-
-// three.addEventListener("click", (event) => {
-//     display.innerHTML += 3;
-// })
-
-// four.addEventListener("click", (event) => {
-//     display.innerHTML += 4;
-// })
-
-// five.addEventListener("click", (event) => {
-//     display.innerHTML += 5;
-// })
-
-// six.addEventListener("click", (event) => {
-//     display.innerHTML += 6;
-// })
-
-// seven.addEventListener("click", (event) => {
-//     display.innerHTML += 7;
-// })
-
-// eight.addEventListener("click", (event) => {
-//     display.innerHTML += 8;
-// })
-
-// nine.addEventListener("click", (event) => {
-//     display.innerHTML += 9;
-// })
-
-// zero.addEventListener("click", (event) => {
-//     display.innerHTML += 0;
-// })
-
-// clear.addEventListener("click", (event) => {
-//     display.innerHTML = " ";
-// })
+total.addEventListener("click", (event) => {
+    
+    secondNumber = display.innerHTML;
+    console.log(secondNumber);
+    if (symbol === "+") {
+        equals = Number(firstNumber) + Number(secondNumber);
+        display.innerHTML = `${firstNumber} ${symbol} ${secondNumber}`;
+        totalDisplay.innerHTML = equals;
+    } else if (symbol === "-") {
+        equals = Number(firstNumber) - Number(secondNumber);
+        display.innerHTML = `${firstNumber} ${symbol} ${secondNumber}`;
+        totalDisplay.innerHTML = equals;
+    } else if (symbol === "*") {
+        equals = Number(firstNumber) * Number(secondNumber);
+        display.innerHTML = `${firstNumber} ${symbol} ${secondNumber}`;
+        totalDisplay.innerHTML = equals;
+    } else if (symbol === "/") {
+        equals = Number(firstNumber) / Number(secondNumber);
+        display.innerHTML = `${firstNumber} ${symbol} ${secondNumber}`;
+        totalDisplay.innerHTML = equals;
+    }
+})
 
 clear.addEventListener("click", (event) => {
     display.innerHTML = " ";
+    firstNumber = ""
+    secondNumber = ""
+    symbol = ""
+    totalDisplay.innerHTML = ""
+})
+
+allClear.addEventListener("click", (event) => {
+    totalDisplay.innerHTML = " ";
 })
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,69 +1,65 @@
 "use strict";
 
 var display = document.getElementById("console__display");
+var totalDisplay = document.getElementById("console__total");
 var numbers = document.querySelectorAll(".btn__number");
-var operators = document.querySelectorAll(".btn__operator"); // const one = document.getElementById("1");
-// const two = document.getElementById("2");
-// const three = document.getElementById("3");
-// const four = document.getElementById("4");
-// const five = document.getElementById("5");
-// const six = document.getElementById("6");
-// const seven = document.getElementById("7");
-// const eight = document.getElementById("8");
-// const nine = document.getElementById("9");
-// const zero = document.getElementById("0");
-
+var operators = document.querySelectorAll(".btn__operator");
 var clear = document.getElementById("clear");
-var firstNumber = 0; // for (let i=0; i<numbers.length; i++) {
-//     numbers[i].addEventListener("click", (event) => {
-//         display.innerHTML += numbers[i].innerHTML;
-//     })
-// }
-
+var allClear = document.getElementById("allClear");
+var total = document.getElementById("total");
+var firstNumber;
+var secondNumber;
+var equals;
+var symbol;
 numbers.forEach(function (number) {
   number.addEventListener("click", function (event) {
     display.innerHTML += number.innerHTML;
+
+    if (firstNumber) {
+      secondNumber = display.innerHTML;
+    } else {
+      firstNumber = display.innerHTML;
+    }
   });
 });
 operators.forEach(function (operator) {
   operator.addEventListener("click", function (event) {
-    firstNumber = display;
-    display.innerHTML += operator.innerHTML;
+    totalDisplay.innerHTML = firstNumber;
+    totalDisplay.innerHTML += operator.innerHTML;
+    console.log(operator.innerHTML);
+    symbol = operator.innerHTML;
+    display.innerHTML = " ";
   });
-}); // one.addEventListener("click", (event) => {
-//     display.innerHTML += 1;
-// })
-// two.addEventListener("click", (event) => {
-//     display.innerHTML += 2;
-// })
-// three.addEventListener("click", (event) => {
-//     display.innerHTML += 3;
-// })
-// four.addEventListener("click", (event) => {
-//     display.innerHTML += 4;
-// })
-// five.addEventListener("click", (event) => {
-//     display.innerHTML += 5;
-// })
-// six.addEventListener("click", (event) => {
-//     display.innerHTML += 6;
-// })
-// seven.addEventListener("click", (event) => {
-//     display.innerHTML += 7;
-// })
-// eight.addEventListener("click", (event) => {
-//     display.innerHTML += 8;
-// })
-// nine.addEventListener("click", (event) => {
-//     display.innerHTML += 9;
-// })
-// zero.addEventListener("click", (event) => {
-//     display.innerHTML += 0;
-// })
-// clear.addEventListener("click", (event) => {
-//     display.innerHTML = " ";
-// })
+});
+total.addEventListener("click", function (event) {
+  secondNumber = display.innerHTML;
+  console.log(secondNumber);
 
+  if (symbol === "+") {
+    equals = Number(firstNumber) + Number(secondNumber);
+    display.innerHTML = "".concat(firstNumber, " ").concat(symbol, " ").concat(secondNumber);
+    totalDisplay.innerHTML = equals;
+  } else if (symbol === "-") {
+    equals = Number(firstNumber) - Number(secondNumber);
+    display.innerHTML = "".concat(firstNumber, " ").concat(symbol, " ").concat(secondNumber);
+    totalDisplay.innerHTML = equals;
+  } else if (symbol === "*") {
+    equals = Number(firstNumber) * Number(secondNumber);
+    display.innerHTML = "".concat(firstNumber, " ").concat(symbol, " ").concat(secondNumber);
+    totalDisplay.innerHTML = equals;
+  } else if (symbol === "/") {
+    equals = Number(firstNumber) / Number(secondNumber);
+    display.innerHTML = "".concat(firstNumber, " ").concat(symbol, " ").concat(secondNumber);
+    totalDisplay.innerHTML = equals;
+  }
+});
 clear.addEventListener("click", function (event) {
   display.innerHTML = " ";
+  firstNumber = "";
+  secondNumber = "";
+  symbol = "";
+  totalDisplay.innerHTML = "";
+});
+allClear.addEventListener("click", function (event) {
+  totalDisplay.innerHTML = " ";
 });
